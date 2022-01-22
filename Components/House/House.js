d27bf40e653fac1fd1373ae5ff44d68675f84b0b
unknown <you@example.com>
2022-01-22 15:22:47 +0600

    first commit

---
+++ Components/House/House.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TopBanner from '../TopBanner/TopBanner';
+
+const House = () => {
+  return (
+    <>
+      <TopBanner />
+    </>
+  );
+};
+
+export default House;
---
+++ Components/TopBanner/TopBanner.js
@@ -0,0 +1,14 @@
+const TopBanner = () => {
+  return (
+    <>
+      <div className="lg:container mx-auto ">
+        <div className="grid lg:grid-cols-2 sm:gird-cols-1 gap-4">
+          <div>01</div>
+          <div>09</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TopBanner;
